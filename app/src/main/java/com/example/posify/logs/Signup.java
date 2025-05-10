d1cc46cd96d7d9c75d4e60430db18325aec548f7
Abhijeet Biswas <abhijeet.24mca10070@vitbhopal.ac.in>
2025-05-10 08:08:29 +0530

    Initial commit

--- app/src/main/java/com/example/posify/logs/Signup.java
+++ app/src/main/java/com/example/posify/logs/Signup.java
@@ -1,9 +1,6 @@
-package com.example.posify;
-
-
+package com.example.posify.logs;
 
 import android.os.Bundle;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.posify.R;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sidebar_drawerlayout), (v, insets) -> {
+        setContentView(R.layout.signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
 }
